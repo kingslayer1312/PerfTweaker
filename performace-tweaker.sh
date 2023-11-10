ec1fc743958bb30a35ca219c4be0fafe6d0210e0
Kingslayer1312 <hrishabari@gmail.com>
2023-11-10 20:09:37 +0530

    completed the shell script

--- performace-tweaker.sh
+++ performace-tweaker.sh
@@ -4,3 +4,26 @@
 modes=($(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors))
 echo "Your CPU supports the following modes: "
 echo ${modes[*]}
+
+echo "Choose a mode: "
+read selected_mode
+
+cd /usr/local/bin/
+echo "#!/bin/bash" >> cpu_gov.sh
+echo "echo $selected_mode | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor"
+chmod 744 /usr/local/bin/cpu_gov.sh
+
+cd /etc/systemd/system/
+echo "[Unit]" >> cpu_gov.service
+echo "After=network.target" >> cpu_gov.service
+echo -en '\n' >> cpu_gov.service
+echo "[Service]" >> cpu_gov.service
+echo "ExecStart=/usr/local/bin/cpu_gov.sh" >> cpu_gov.service
+echo -en '\n' >> cpu_gov.service
+echo "[Install]" >> cpu_gov.service
+echo "WantedBy=default.target" >> cpu_gov.service
+chmod 664 /etc/systemd/system/cpu_gov.service
+
+systemctl daemon-reload
+systemctl enable perf-prof.service
+systemctl start perf-prof.service
